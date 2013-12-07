fbf23535384b4b590a76f8b3f68770da785bc961
Yuhei Omori <yuhei.omori@beproud.jp>
2013-12-07 17:04:05 +0900

    add go path

--- conf/exec-path.el
+++ conf/exec-path.el
@@ -1,7 +1,8 @@
-(setq shell-file-name "/bin/sh")
+(setq shell-file-name "/bin/zsh")
 
 (let*
     ((path (list "/usr/local/bin"
+                 "/usr/local/go/bin"
                  "~/dev/go_lib/bin")))
   (dolist (p path)
     (add-to-list 'exec-path (expand-file-name p))
